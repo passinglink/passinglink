aa7633a556a216e48534baef804e144834d3cdb2
Josh Gao <josh@jmgao.dev>
2020-11-09 21:15:07 -0800

    Bluetooth cleanup.

--- src/bt/bt.cpp
+++ src/bt/bt.cpp
@@ -16,15 +16,14 @@
 #define LOG_LEVEL LOG_LEVEL_INF
 LOG_MODULE_REGISTER(bt);
 
-static const struct bt_data pl_bt_adv[] = {
-    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-    BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0x0d, 0x18, 0x0f, 0x18, 0x05, 0x18),
-    BT_DATA_BYTES(BT_DATA_UUID128_ALL, 0x00, 0x00, 0x00, 0x00, PL_BT_UUID_PREFIX),
-};
-
-static struct bt_le_adv_param pl_bt_adv_params =
+static const struct bt_le_adv_param pl_bt_adv_params =
     BT_LE_ADV_PARAM_INIT(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_USE_NAME,
                          BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, nullptr);
+
+static const struct bt_data pl_bt_adv_data[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, 0x00, 0x00, PL_BT_UUID_PREFIX),
+};
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
@@ -47,13 +46,13 @@ static struct bt_conn_cb connection_cbs = {
 void bluetooth_init() {
   int err = bt_enable(nullptr);
   if (err != 0) {
-    LOG_ERR("initialization failed: error = %02x", err);
+    LOG_ERR("initialization failed: error = %d", err);
     return;
   }
 
   err = bt_set_name("Passing Link");
   if (err != 0) {
-    LOG_ERR("bt_set_name failed: error = %02x", err);
+    LOG_ERR("bt_set_name failed: error = %d", err);
     return;
   }
 
@@ -62,7 +61,7 @@ void bluetooth_init() {
   static_assert(CONFIG_PASSINGLINK_BT_PAIRING_KEY <= 999999, "BT pairing key must be 6 digits");
   err = bt_passkey_set(CONFIG_PASSINGLINK_BT_PAIRING_KEY);
   if (err != 0) {
-    LOG_ERR("bt_passkey_set failed: error = %02x", err);
+    LOG_ERR("bt_passkey_set failed: error = %d", err);
     return;
   } else {
     LOG_INF("Bluetooth passkey set to %d", CONFIG_PASSINGLINK_BT_PAIRING_KEY);
@@ -70,9 +69,9 @@ void bluetooth_init() {
 
   bt_conn_cb_register(&connection_cbs);
 
-  err = bt_le_adv_start(&pl_bt_adv_params, pl_bt_adv, ARRAY_SIZE(pl_bt_adv), nullptr, 0);
+  err = bt_le_adv_start(&pl_bt_adv_params, pl_bt_adv_data, ARRAY_SIZE(pl_bt_adv_data), nullptr, 0);
   if (err) {
-    LOG_ERR("advertising failed to start: error = 0x%02x", err);
+    LOG_ERR("advertising failed to start: error = %d", err);
     return;
   }
 }
