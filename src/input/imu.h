fe4f0065836a330c80f99c2452846c68a6f91fb8
dogtopus <dogtopus@users.noreply.github.com>
2020-11-20 16:01:16 -0400

    Seems that most IMUs use 16-bit internal precision. Change the vec3 to 16-bit

--- src/input/imu.h
+++ src/input/imu.h
@@ -2,26 +2,26 @@
 
 #include <types.h>
 
-struct IMUVec3_32 {
+struct IMUVec3_16 {
   union {
     struct {
-      int32_t x;
-      int32_t y;
-      int32_t z;
+      int16_t x;
+      int16_t y;
+      int16_t z;
     };
     struct {
-      int32_t pitch;
-      int32_t yaw;
-      int32_t roll;
+      int16_t pitch;
+      int16_t yaw;
+      int16_t roll;
     };
   };
 };
 
 struct InternalIMUData {
   uint32_t gyro_timestamp;
-  IMUVec3_32 gyro;
+  IMUVec3_16 gyro;
   uint32_t accel_timestamp;
-  IMUVec3_32 accel;
+  IMUVec3_16 accel;
 };
 
 extern InternalIMUData imu_data;
