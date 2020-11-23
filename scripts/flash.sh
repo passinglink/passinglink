82478a8130a9580b1e6a4d832b7fc524d6b1d4c2
Josh Gao <josh@jmgao.dev>
2020-11-23 00:57:14 -0800

    Fix CI.

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -8,6 +8,11 @@ ROOT="$SCRIPT_PATH/../.."
 set -euxo pipefail
 
 if [[ $PL_MCUBOOT_SUPPORTED == "1" ]] || [[ -n $PL_PROVISIONING_OFFSET ]]; then
+  if ! pyocd list -t | egrep -q "^\s+$PL_PYOCD_TYPE\s"; then
+    echo "pyocd support missing for '$PL_PYOCD_TYPE': try \`pyocd pack --install $PL_PYOCD_TYPE\`?"
+    exit 1
+  fi
+
   pyocd flash -e sector -a 0x0 -t $PL_PYOCD_TYPE "$BUILD_DIR/mcuboot.bin"
   pyocd flash -e sector -a 0x10000 -t $PL_PYOCD_TYPE "$BUILD_DIR/pl.bin"
 else
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -14,6 +14,11 @@ set -euxo pipefail
 
 mkdir -p "$BUILD_DIR"
 
+if ! pyocd list -t | egrep -q "^\s+$PL_PYOCD_TYPE\s"; then
+  echo "pyocd support missing for '$PL_PYOCD_TYPE': try \`pyocd pack --install $PL_PYOCD_TYPE\`?"
+  exit 1
+fi
+
 "$ROOT/tools/mbed_embed/mbed_embed" $PL_PROVISIONING_OFFSET $PL_PROVISIONING_SIZE "$1" "$2" "$3" "$4" > \
   "$BUILD_DIR/provisioning.bin"
 pyocd flash -e sector -a $PL_PROVISIONING_OFFSET -t $PL_PYOCD_TYPE $BUILD_DIR/provisioning.bin
