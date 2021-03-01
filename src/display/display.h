6c823c7ea82684560f3465c793b9d8477981a4ab
Josh Gao <josh@jmgao.dev>
2021-03-01 01:38:58 -0800

    bootloader: support display.

--- src/display/display.h
+++ src/display/display.h
@@ -3,20 +3,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "display/ssd1306.h"
 #include "output/usb/probe_type.h"
 
-// Columns of text
-#define DISPLAY_WIDTH 21
-
-// Rows of text
-#define DISPLAY_ROWS 3
-
 void display_init();
-
-// Display primitives
-void display_set_line(size_t row_idx, const char* line);
-void display_draw_logo();
-void display_blit();
 
 void display_update_latency(uint32_t ticks);
 void display_set_locked(bool locked);
--- src/display/ssd1306.h
+++ src/display/ssd1306.h
@@ -1,3 +1,22 @@
 #pragma once
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+// Columns of text
+#define DISPLAY_WIDTH 21
+
+// Rows of text
+#define DISPLAY_ROWS 3
+
 bool ssd1306_init();
+
+// Display primitives
+void display_set_line(size_t row_idx, const char* line);
+void display_draw_logo();
+void display_blit();
+
+#if defined(__cplusplus)
+}
+#endif
